57644536930ff7c316ff93c85369c3a9f1f4da3f
Karim Houbbi <k.houbbi29@gmail.com>
2021-05-10 14:53:35 +0200

    Modification de la fonction MoveCube.cs

--- Assets/Script/MoveCube.cs
+++ Assets/Script/MoveCube.cs
@@ -5,17 +5,49 @@ using UnityEngine;
 public class MoveCube : MonoBehaviour
 {
     public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 100f;
+        moveSpeed = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,   //gauche & droite
-            moveSpeed*Input.GetAxis("Jump")*Time.deltaTime,                         // monter
-            moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);                    // haut & bas
+        // Monte le joueur sur l'axe Y
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.transform.Translate(Vector3.up * moveSpeed);
+        }
+
+        // Descends le joueur sur l'axe Y
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            this.transform.Translate(Vector3.down * moveSpeed);
+        }
+
+        // Fais aller le joueur sur la gauche
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * moveSpeed);
+        }
+
+        // Fais aller le joueur sur la droite
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * moveSpeed);
+        }
+        // Fais aller le joueur en avant
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * moveSpeed);
+        }
+
+        // Fais aller le joueur à reculon
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * moveSpeed);
+        }
     }
 }
