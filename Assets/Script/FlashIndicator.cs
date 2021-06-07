5f47e2de34e7ef9bb24d37529383d38f21a0948e
Retti Ubaldo <Ubaldo.Retti@divtec.ch>
2021-06-07 10:58:42 +0200

    Maj

---
+++ Assets/Script/FlashIndicator.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashIndicator : MonoBehaviour
+{
+
+    private Renderer currenIndicator;
+    private Coroutine flashIndicator;
+    public MoveIndicator moveIndicator;
+    public GameObject rightIndicator;
+    public GameObject leftIndicator;
+    private bool isRightIndicatorCurrent = true;
+    private bool continueFlash = true;
+
+    private GameObject indicatorCurrent;
+
+    private Renderer colorIndicatorCurrent;
+
+    /// <summary>
+    /// Appelle la fonction qui fera clignoter les indicateurs
+    /// </summary>
+    /// <param name="indicatorCurrent"></param>
+    public void flashCurrentIndicator(GameObject indicatorCurrent)
+    {
+        Color currentColor = moveIndicator.getCurrentColorIndicator();
+
+        colorIndicatorCurrent = indicatorCurrent.GetComponent<Renderer>();
+        
+        flashIndicator = StartCoroutine(FlashCurrentIndicator(currentColor));
+    }
+
+    /// <summary>
+    /// Fais clignoter l'indicateur courant
+    /// </summary>
+    /// <param name="currentColor"></param>
+    /// <returns></returns>
+    IEnumerator FlashCurrentIndicator(Color currentColor)
+    {
+
+        continueFlash = true;
+
+        while (continueFlash)
+        {
+            
+            yield return new WaitForSeconds(0.3f);
+            colorIndicatorCurrent.material.SetColor("_Color", currentColor);
+            
+            yield return new WaitForSeconds(0.3f);
+            colorIndicatorCurrent.material.SetColor("_Color", Color.white);
+        }
+
+        yield return null;
+
+    }
+    /// <summary>
+    /// Arrete le clignotement
+    /// </summary>
+    public void stopFlashCurrentIndicator()
+    {
+        
+        continueFlash = false;
+        StopCoroutine(flashIndicator);
+    }
+    
+}
---
+++ Assets/Script/ShrinkIndicator.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ShrinkIndicator : MonoBehaviour
+{
+    private const float MIN_DIAMETER_INDICATOR = 8.0f;
+
+
+    public GameObject rightIndicator;
+    public GameObject leftIndicator;
+
+    public FlashIndicator flashIndicator;
+
+
+    private bool isRightIndicatorCurrent = true;
+
+    private Coroutine coroutineShrinkIndicator;
+
+    private Vector3 scaleCircleIndicator;
+
+
+    private void Start()
+    {
+        // Rétrécis le premier indicator jusqu'à la limite
+        shrinkCurrentIndicator();
+    }
+
+
+    /// <summary>
+    /// Rétrécis l'indicateur courrant à l'aide de la fonction StartCoroutine (qui permet de
+    /// de rétrécir l'indicateur avec fluidité et non que l'indicateur se rétrécit immédiatement) 
+    /// </summary>
+    public void shrinkCurrentIndicator()
+    {
+        GameObject indicatorCurrent;
+
+
+        // Test de qui est l'indicateur courrant     
+        if (isRightIndicatorCurrent)
+        {
+            indicatorCurrent = rightIndicator;
+            isRightIndicatorCurrent = false;
+        }
+        else
+        {
+            indicatorCurrent = leftIndicator;
+            isRightIndicatorCurrent = true;
+        }
+
+        scaleCircleIndicator = indicatorCurrent.gameObject.transform.localScale;
+
+        // Démarre la fonction rétrécit l'indicateur courrant
+        coroutineShrinkIndicator = StartCoroutine(ScaleToTargetCoroutine(indicatorCurrent));
+    }
+
+
+    /// <summary>
+    /// Rétrécis l'indicateur courrant
+    /// </summary>
+    /// <param name="indicator"> L'indicateur à rétrécir </param>
+    /// <returns> null </returns>
+    private IEnumerator ScaleToTargetCoroutine(GameObject indicator)
+    {
+        while (scaleCircleIndicator.x > MIN_DIAMETER_INDICATOR)
+        {
+            indicator.gameObject.transform.localScale -= new Vector3(0.05f, 0.0f, 0.05f);
+
+            scaleCircleIndicator = indicator.gameObject.transform.localScale;
+
+            yield return null;
+        }
+
+        flashIndicator.flashCurrentIndicator(indicator);
+
+        yield return null;
+    }
+
+
+    /// <summary>
+    /// Remets à l'échelle de base l'indicateur
+    /// </summary>
+    /// <param name="indicator">  L'indicateur à remettre à l'échelle de base </param>
+    public void setDefaultSizeIndicator()
+    {
+        GameObject indicatorNext;
+
+        // Test de qui est l'indicateur prochain
+        if (isRightIndicatorCurrent)
+        {
+            indicatorNext = leftIndicator;
+        }
+        else
+        {
+            indicatorNext = rightIndicator;
+        }
+
+
+        indicatorNext.gameObject.transform.localScale = new Vector3(15.0f, 1.0f, 15.0f);
+    }
+
+
+    /// <summary>
+    /// Mets la valeur à vrai si le bouton à été appuyé
+    /// </summary>
+    /// <param name="pressed"> valeur vraie si le bouton est pressé sinon faux </param>
+    public void stopShrinkIndicator()
+    {
+        StopCoroutine(coroutineShrinkIndicator);
+    }
+}
