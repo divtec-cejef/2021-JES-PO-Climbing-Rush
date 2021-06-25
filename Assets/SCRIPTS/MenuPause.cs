0be00441234ab8e31fac70bbf265e0e35b32caad
Maxime Christe <maxime.christe@divtec.ch>
2021-06-25 13:25:07 +0200

    maj

---
+++ Assets/SCRIPTS/MenuPause.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject menuPause;
+    public GameObject HUD;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+
+    public void ResumeGame()
+    {
+        HUD.SetActive(true);
+        menuPause.SetActive(false);
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+    }
+
+    public void PauseGame()
+    {
+        HUD.SetActive(false);
+        menuPause.SetActive(true);
+        Time.timeScale = 0.0f;
+        isGamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
