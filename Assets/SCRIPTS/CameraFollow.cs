73316d00edabe213e70536eed20468b8f3cf1ba2
Maxime Christe <maxime.christe@divtec.ch>
2021-06-28 20:22:26 +0200

    Ajout d'une caméra qui suit le joueur

--- Assets/SCRIPTS/CameraFollow.cs
+++ Assets/SCRIPTS/CameraFollow.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform targetPlayer;
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
@@ -14,20 +15,9 @@ public class CameraFollow : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Vector3 targetPosition = targetPlayer.TransformPoint(offset);
         
-        //Vector3 targetPosition = target.position + offset;
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -6));
-        
-        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
-        //transform.position = smoothedPosition;
-        
-        
-        // Define a target position above and behind the target transform
-        //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
-
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
-        
-        
     }
 }
