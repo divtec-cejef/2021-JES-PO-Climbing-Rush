dcbb305435ae4439f220ea30871220b61c8e096d
Maxime Christe <maxime.christe@divtec.ch>
2021-08-28 16:47:16 +0200

    modification mineure

---
+++ Assets/SCRIPTS/Display2Camera.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class Display2Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Premet de changé la taille de la fenetre
+        for (int i  = 0; i < Display.displays.Length;i++)
+        {
+            Display.displays[i].Activate(1920,1080,60);
+        }
+        
+    }
+}
