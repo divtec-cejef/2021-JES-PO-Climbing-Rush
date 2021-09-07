96f57e3a39381df4dce719234ebffaf9ac55768b
Maxime Christe <maxime.christe@divtec.ch>
2021-09-07 20:58:09 +0200

    Fais pour que la caméra des deux joueurs se splitent à la fin de la grande cinématique. Et désactiver l'HUD, etc pendant la cinématique

--- Assets/SCRIPTS/CameraCinematique.cs
+++ Assets/SCRIPTS/CameraCinematique.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,13 +8,28 @@ public class CameraCinematique : MonoBehaviour
 {
     [SerializeField] private Image IndicatorPlayer1;
     [SerializeField] private Image IndicatorPlayer2;
-    [SerializeField] private 
-    
+    [SerializeField] private Camera cameraPlay1;
+    [SerializeField] private Camera cameraPlay2;
+    [SerializeField] private Camera cameraCinematicPlayer2;
+    [SerializeField] private Canvas HUDPlayer1;
+    [SerializeField] private Canvas HUDPlayer2;
+    [SerializeField] private TextMeshProUGUI positionOfPlayer1;
+    [SerializeField] private TextMeshProUGUI positionOfPlayer2;
     
     void Start()
     {
         IndicatorPlayer1.gameObject.SetActive(false);
         IndicatorPlayer2.gameObject.SetActive(false);
+        
+        cameraPlay1.gameObject.SetActive(false);
+        cameraPlay2.gameObject.SetActive(false);
+        
+        HUDPlayer1.gameObject.SetActive(false);
+        HUDPlayer2.gameObject.SetActive(false);
+        
+        positionOfPlayer1.gameObject.SetActive(false);
+        positionOfPlayer2.gameObject.SetActive(false);
+    
     }
 
     // Update is called once per frame
@@ -21,5 +37,19 @@ public class CameraCinematique : MonoBehaviour
     {
         IndicatorPlayer1.gameObject.SetActive(true);
         IndicatorPlayer2.gameObject.SetActive(true);
+        
+        this.gameObject.SetActive(false);
+        cameraCinematicPlayer2.gameObject.SetActive(false);
+        
+        cameraPlay1.gameObject.SetActive(true);
+        cameraPlay2.gameObject.SetActive(true);
+        
+        
+        HUDPlayer1.gameObject.SetActive(true);
+        HUDPlayer2.gameObject.SetActive(true);
+        
+        positionOfPlayer1.gameObject.SetActive(true);
+        positionOfPlayer2.gameObject.SetActive(true);
+        
     }
 }
